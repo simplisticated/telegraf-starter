f21a007f84bcc9f58d62821369e50c589cbcb96b
Igor <simplisticated.software@gmail.com>
2025-04-05 07:55:05 +0300

    ...

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import "reflect-metadata";
 import STORE from "./data/store/store";
 
 async function start(): Promise<Telegraf> {
-    await STORE.initialize();
-
     if (!ENV.TELEGRAM_TOKEN) {
         throw new Error("Telegram token not found");
     }
+
+    await STORE.initialize();
+
     const bot = new Telegraf(ENV.TELEGRAM_TOKEN);
     MIDDLEWARE_LIST.forEach(middleware => bot.use(middleware));
     bot.launch();
