b20598cc2417ae28f2276e0cbbc75fc756ac28f4
Igor <simplisticated.software@gmail.com>
2025-11-28 04:51:53 +0300

    ...

--- src/middleware/command.ts
+++ src/middleware/command.ts
@@ -12,10 +12,13 @@ export default async function handleCommand(
         const segments = context.message.text.split(" ");
         const command = segments[0].slice(1);
         switch (command) {
-            default: {
+            case "start": {
                 await context.scene.leave();
                 await context.scene.enter("start-scene");
                 return;
+            }
+            default: {
+                break;
             }
         }
     }
